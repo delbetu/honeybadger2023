ec3ce05863db86dc896d207c9d20d92192d87392
M. Bellucci <delbetu@gmail.com>
2023-03-18 19:01:36 -0300

    Implement MessagesController#create
    pending to create SpamChecker

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,51 +1,26 @@
+# frozen_string_literal: true
+
+# Receives messages and delegates to spam checker.
 class MessagesController < ApplicationController
-  before_action :set_message, only: %i[ show update destroy ]
-
-  # GET /messages
-  def index
-    @messages = Message.all
-
-    render json: @messages
-  end
-
-  # GET /messages/1
-  def show
-    render json: @message
-  end
-
   # POST /messages
   def create
-    @message = Message.new(message_params)
+    result = SpamChecker.process(message_params)
 
-    if @message.save
-      render json: @message, status: :created, location: @message
+    if result.success
+      render json: { status: 'success', message: 'message processed' }, status: :created
     else
-      render json: @message.errors, status: :unprocessable_entity
+      render json: { status: 'error', message: result.errors.to_sentence }, status: :unprocessable_entity
     end
-  end
-
-  # PATCH/PUT /messages/1
-  def update
-    if @message.update(message_params)
-      render json: @message
-    else
-      render json: @message.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /messages/1
-  def destroy
-    @message.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_message
-      @message = Message.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def message_params
-      params.fetch(:message, {})
-    end
+  # Only allow a list of trusted parameters through.
+  def message_params
+    params.permit(
+      :record_type, :message_type, :type_code,
+      :name, :tag, :message_stream,
+      :description, :email, :from, :bounced_at
+    )
+  end
 end
